637f580a592d24e1631aa926d3a67f560f8dc831
Havil B <havilaxz07@gmail.com>
2026-01-02 02:32:30 -0500

    Scope practice

---
+++ Scope/scope_lab.js
@@ -0,0 +1,16 @@
+var nameGlobal = "havil";
+let ageGlobal = 30;
+const genderGlobal = "Male";
+
+{
+    var hobby = "Reading";
+    let edu = "BE";
+    const married = "No";
+    
+}
+console.log(nameGlobal);
+console.log(ageGlobal);
+console.log(genderGlobal);
+
+console.log(edu);
+console.log(married);
